74ea80988bcf542abc8676163a7e17ddbf1da9aa
Haoran Chen <dr.hrchen@gmail.com>
2015-04-12 20:27:06 +0800

    rename project.

---
+++ wifi-ios/Wifi/Wifi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Wifi
+//
+//  Created by Haoran Chen on 4/12/15.
+//  Copyright (c) 2015 KiloApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ wifi-ios/Wifi/Wifi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Wifi
+//
+//  Created by Haoran Chen on 4/12/15.
+//  Copyright (c) 2015 KiloApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
