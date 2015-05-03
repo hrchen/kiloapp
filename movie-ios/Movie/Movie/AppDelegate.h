52bf855b79294d301fef292574eced66caff85d1
Haoran Chen <dr.hrchen@gmail.com>
2015-05-03 23:43:21 +0800

    update

---
+++ movie-ios/Movie/Movie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Movie
+//
+//  Created by Haoran Chen on 5/1/15.
+//  Copyright (c) 2015 KiloApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
