c3846ae48e17be8770719640b684248df728521d
chenhr <chenhr@Ctrip.com>
2015-05-15 01:27:12 +0800

    Update

---
+++ movie-ios/Movie/Movie/UI/Root/DMTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  DMTableViewController.h
+//  Movie
+//
+//  Created by Haoran Chen on 5/12/15.
+//  Copyright (c) 2015 KiloApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DMTableViewController : UITableViewController
+
+@end
