0738184d86fd7cd1e48309c5a2689b6aa652331a
chenhr <chenhr@Ctrip.com>
2015-07-22 21:34:06 +0800

    more check in.

--- movie-ios/Movie/Movie/Models/DMComment.h
+++ movie-ios/Movie/Movie/Models/DMComment.h
@@ -10,4 +10,15 @@
 
 @interface DMComment : NSObject
 
+@property (nonatomic, readwrite, assign) NSUInteger commentID;
+
+@property (nonatomic, readwrite, copy) NSString *content;
+
+@property (nonatomic, readwrite, assign) long postAt;
+
+@property (nonatomic, readwrite, assign) BOOL isUp;
+
+@property (nonatomic, readwrite, assign) long upAmount;
+
+
 @end
--- movie-ios/Movie/Movie/Models/DMMovie.h
+++ movie-ios/Movie/Movie/Models/DMMovie.h
@@ -10,4 +10,22 @@
 
 @interface DMMovie : NSObject
 
+@property (nonatomic, readwrite, assign) NSUInteger movieID;
+
+@property (nonatomic, readwrite, copy) NSString *title;
+
+@property (nonatomic, readwrite, assign) NSUInteger duration;
+
+@property (nonatomic, readwrite, copy) NSString *type;
+
+@property (nonatomic, readwrite, copy) NSString *director;
+
+@property (nonatomic, readwrite, copy) NSString *cast;
+
+@property (nonatomic, readwrite, copy) NSString *posterURL;
+
+@property (nonatomic, readwrite, copy) NSString *story;
+
+@property (nonatomic, readwrite, assign) NSUInteger rank;
+
 @end
