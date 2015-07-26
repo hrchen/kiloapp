6c4b8f85cc9b8091b4fcca644b711f3c0c16ec4f
Haoran Chen <dr.hrchen@gmail.com>
2015-07-26 23:45:54 +0800

    add datasource

---
+++ movie-ios/Movie/Movie/UI/Home/DMDataSource.h
@@ -0,0 +1,15 @@
+//
+//  DMDataSource.h
+//  Movie
+//
+//  Created by Haoran Chen on 7/26/15.
+//  Copyright (c) 2015 KiloApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DMDataSource : NSObject <UITableViewDataSource>
+
+
+
+@end
---
+++ movie-ios/Movie/Movie/UI/Home/DMDataSource.m
@@ -0,0 +1,13 @@
+//
+//  DMDataSource.m
+//  Movie
+//
+//  Created by Haoran Chen on 7/26/15.
+//  Copyright (c) 2015 KiloApp. All rights reserved.
+//
+
+#import "DMDataSource.h"
+
+@implementation DMDataSource
+
+@end
