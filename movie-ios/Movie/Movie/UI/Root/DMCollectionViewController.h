fca2e517a64fb875b0f3df78b3b0aad5d0849954
Haoran Chen <dr.hrchen@gmail.com>
2015-08-17 07:46:51 +0800

    Update

---
+++ movie-ios/Movie/Movie/UI/Root/DMCollectionViewController.h
@@ -0,0 +1,15 @@
+//
+//  DMCollectionViewController.h
+//  Movie
+//
+//  Created by Haoran Chen on 8/16/15.
+//  Copyright (c) 2015 KiloApp. All rights reserved.
+//
+
+#import "DMViewController.h"
+
+@interface DMCollectionViewController : DMViewController
+
+@property (nonatomic, readwrite, retain) UICollectionView *collectionView;
+
+@end
